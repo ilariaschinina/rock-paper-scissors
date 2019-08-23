7cbde4236d26a2079545ea235f7d9f6b73076e58
Ilaria-Schinina <ilaria.schinina@gmail.com>
2019-08-23 17:39:51 +0200

    Style code adjustments

--- rock-paper-scissors.py
+++ rock-paper-scissors.py
@@ -7,19 +7,9 @@ import random
 moves = ['rock', 'paper', 'scissors']
 
 
-class Player:
-    '''The Player class is the parent class for all of the Players
-    in this game'''
-    score = 0
-    
-    def move(self):
-        return 'rock'
-
-    def learn(self, my_move, their_move):
-        pass
-
 def beats(move1, move2):
     ''' Returns True if player1 wins this round, False otherwise'''
+
     return ((move1 == 'rock' and move2 == 'scissors') or
             (move1 == 'scissors' and move2 == 'paper') or
             (move1 == 'paper' and move2 == 'rock'))
@@ -34,6 +24,18 @@ def valid_input(prompt):
                 return response
 
 
+class Player:
+    '''The Player class is the parent class for all of the Players
+    in this game'''
+    score = 0
+
+    def move(self):
+        return 'rock'
+
+    def learn(self, my_move, their_move):
+        pass
+
+
 class RandomPlayer(Player):
     '''Computer Player that chooses its move at random.'''
     def move(self):
@@ -42,37 +44,39 @@ class RandomPlayer(Player):
 
 
 class HumanPlayer(Player):
-    '''HumanPlayer is played by the user and the move method asks the user's input.'''
+    '''HumanPlayer is played by the user'''
     def move(self):
         move = valid_input("Rock, paper, scissors? > ")
         return move
 
 
 class ReflectPlayer(Player):
-    '''ReflectPlayer remembers what move the opponent played last round, 
+    '''ReflectPlayer remembers what move the opponent played last round,
     and plays that move this round.'''
 
     def __init__(self):
         self.opponent_move = 'rock'
-    
+
     def learn(self, my_move, their_move):
         self.opponent_move = their_move
-    
+
     def move(self):
         move = self.opponent_move
         return move
 
+
 class CyclePlayer(Player):
-    '''CyclePlayer remembers what move it played last round, 
+    '''CyclePlayer remembers what move it played last round,
     and cycles through the different moves.'''
+
     def __init__(self):
         self.last_move = None
 
     def learn(self, my_move, their_move):
         self.last_move = my_move
 
-    def move (self):
-        if self.last_move == None:
+    def move(self):
+        if self.last_move is None:
             move = random.choice(moves)
             return move
         elif self.last_move == moves[2]:
@@ -80,7 +84,7 @@ class CyclePlayer(Player):
             return move
         else:
             index = moves.index(self.last_move)
-            index =+1
+            index += 1
             move = moves[index]
             return move
 
@@ -97,33 +101,32 @@ class Game:
             print(f"Player A played {move1}\nPlayer B played {move2}")
             if beats(move1, move2):
                 print("Player A wins")
-                self.p1.score +=1
+                self.p1.score += 1
                 break
             elif move1 == move2:
                 print("Draw... Replay this round")
                 continue
             else:
                 print("Player B wins")
-                self.p2.score +=1
+                self.p2.score += 1
                 break
         self.p1.learn(move1, move2)
         self.p2.learn(move2, move1)
-
 
     def play_game(self):
         print("Game start!\n")
         for round in range(3):
             print(f"Round {round+1}:")
             self.play_round()
-            print(f"SCORE: Player A: {self.p1.score}\tPlayer B: {self.p2.score}\n")
+            print("SCORE: Player A: {}\tPlayer B: {}\n"
+                  .format(self.p1.score, self.p2.score))
         if self.p1.score > self.p2.score:
             print("***PLAYER A is the WINNER***")
         else:
             print("***PLAYER B is the WINNER***")
         print("Game over!")
-    
+
 
 if __name__ == '__main__':
     game = Game(HumanPlayer(), ReflectPlayer())
     game.play_game()
-
